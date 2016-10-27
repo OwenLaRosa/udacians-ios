732ea5fa541cecfdf4619c8866d420f7aeec73ae
OwenLaRosa <owen.larosa@outlook.com>
2016-10-27 11:52:25 -0400

    feat: Create layout for login screen

---
+++ Udacians/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Udacians
+//
+//  Created by Owen LaRosa on 10/27/16.
+//  Copyright © 2016 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+import MarkupKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var loginLabel: UILabel!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var noticeLabel: UILabel!
+    
+    override func loadView() {
+        view = LMViewBuilder.view(withName: "LoginViewController", owner: self, root: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func login() {
+        
+    }
+
+}
+
