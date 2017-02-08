a42a289e88bc47532a622f7cb7662e7446a7c756
OwenLaRosa <owen.larosa@outlook.com>
2017-02-08 17:03:54 -0500

    feat: Add table for feed posts with placeholder cells

--- Udacians/FeedViewController.swift
+++ Udacians/FeedViewController.swift
@@ -7,7 +7,28 @@
 //
 
 import UIKit
+import MarkupKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var feedTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        feedTableView.rowHeight = UITableViewAutomaticDimension
+        feedTableView.estimatedRowHeight = 140
+        feedTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostTableViewCell") as! PostTableViewCell
+        
+        return cell
+    }
     
 }
---
+++ Udacians/PostTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PostTableViewCell.swift
+//  Udacians
+//
+//  Created by Owen LaRosa on 2/7/17.
+//  Copyright © 2017 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+import MarkupKit
+
+class PostTableViewCell: LMTableViewCell {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+    @IBOutlet var contentImageView: UIImageView!
+    
+}
