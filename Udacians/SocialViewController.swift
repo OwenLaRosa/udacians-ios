f7d2e37ad31786e71604002364e1a99e8609e0e2
OwenLaRosa <owen.larosa@outlook.com>
2017-02-10 22:30:17 -0500

    feat: Create layout for social VC

--- Udacians/SocialViewController.swift
+++ Udacians/SocialViewController.swift
@@ -10,4 +10,70 @@ import UIKit
 
 class SocialViewController: UIViewController {
     
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let chatsTableViewDataSource = ChatsTableViewDataSource()
+    let connectionsTableViewDataSource = ConnectionsTableViewDataSource()
+    let followersTableViewDataSource = FollowersTableViewDataSource()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = chatsTableViewDataSource
+    }
+    
+    @IBAction func segmentValueChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0: // chats
+            tableView.dataSource = chatsTableViewDataSource
+            break
+        case 1: // connections
+            tableView.dataSource = connectionsTableViewDataSource
+            break
+        case 2: // followers
+            tableView.dataSource = followersTableViewDataSource
+            break
+        default:
+            return
+        }
+    }
+    
+}
+
+class ChatsTableViewDataSource: NSObject, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+class ConnectionsTableViewDataSource: NSObject, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+class FollowersTableViewDataSource: NSObject, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
