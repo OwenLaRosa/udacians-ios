b57dd7f3cb94efc18ab1f929c5685ed7c859e1cb
OwenLaRosa <owen.larosa@outlook.com>
2017-02-16 17:02:29 -0500

    feat: Assign references and download title

--- Udacians/MessageViewController.swift
+++ Udacians/MessageViewController.swift
@@ -7,10 +7,24 @@
 //
 
 import UIKit
+import Firebase
 
 class MessageViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBOutlet weak var textEntry: UITextView!
+    
+    @IBOutlet weak var sendButton: UIButton!
+    
+    var userId = "3050228546"
+    
+    var ref: FIRDatabaseReference!
+    var messagesReference: FIRDatabaseReference!
+    var senderDirectMessageReference: FIRDatabaseReference!
+    var recipientDirectMessageReference: FIRDatabaseReference!
     
     var isDirect = false
     var chatId = ""
@@ -20,7 +34,51 @@ class MessageViewController: UIViewController {
         
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 140
+        
+        ref = FIRDatabase.database().reference()
+        var chatTitleReference: FIRDatabaseReference
+        if isDirect {
+            messagesReference = getDirectChatReference(user1: userId, user2: chatId)
+            senderDirectMessageReference = ref.child("users").child(userId).child("direct_messages").child(chatId)
+            recipientDirectMessageReference = ref.child("users").child(chatId).child("direct_messages").child(userId)
+            chatTitleReference = ref.child("users").child(userId).child("basic").child("name")
+        } else {
+            messagesReference = ref.child("topics").child(chatId).child("messages")
+            if chatId.hasPrefix("nd") && !chatId.hasSuffix("beta") {
+                // course discussion for nanodegree
+                chatTitleReference = ref.child("nano_degrees").child(chatId).child("name")
+            } else if chatId.hasSuffix("beta") {
+                // course discussion for beta nanodegree program
+                chatTitleReference = ref.child("nano_degrees").child(chatId.replacingOccurrences(of: "beta", with: "")).child("name")
+            } else {
+                // user posted discussion topic
+                chatTitleReference = ref.child("topics").child(chatId).child("info").child("name")
+            }
+            chatTitleReference.observeSingleEvent(of: .value, with: { (snapshot) in
+                if let title = snapshot.value as? String {
+                    self.title = title
+                } else {
+                    self.title = "Course Discussion"
+                }
+            })
+        }
     }
+    
+    @IBAction func sendButtonTapped(_ sender: UIButton) {
+    }
+    
+    private func getDirectChatReference(user1: String, user2: String) -> FIRDatabaseReference {
+        let chatReference: FIRDatabaseReference
+        let directMessagesReference = ref.child("direct_messages")
+        let user1Int = Int(user1)!
+        let user2Int = Int(user2)!
+        if user1Int < user2Int {
+            chatReference = directMessagesReference.child(user1).child(user2)
+        } else {
+            chatReference = directMessagesReference.child(user2).child(user1)
+        }
+        return chatReference
+     }
     
 }
 
