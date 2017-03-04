7720c35bb4e002f5ca9dc84b8a24b82e7eef75e8
OwenLaRosa <owen.larosa@outlook.com>
2017-03-03 20:22:12 -0500

    fix: Table header layout, autoresize height

--- Udacians/MeViewController.swift
+++ Udacians/MeViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import Firebase
 
-class MeViewController: UIViewController {
+class MeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var profileImageView: UIImageView!
     
@@ -30,27 +32,32 @@ class MeViewController: UIViewController {
         super.viewDidLoad()
         
         ref = FIRDatabase.database().reference()
+        updateTableHeaderHeight()
         
         let userRef = ref.child("users").child(userId)
         let userBasicRef = userRef.child("basic")
         let nameRef = userBasicRef.child("name")
         nameRef.observeSingleEvent(of: .value, with: {(snapshot) in
             self.usernameLabel.text = snapshot.value as? String ?? ""
+            self.updateTableHeaderHeight()
         })
         let titleRef = userBasicRef.child("title")
         titleRef.observeSingleEvent(of: .value, with: {(snapshot) in
             self.titleLabel.text = snapshot.value as? String ?? ""
+            self.updateTableHeaderHeight()
         })
         let photoRef = userBasicRef.child("photo")
         photoRef.observeSingleEvent(of: .value, with: {(snapshot) in
             if let url = snapshot.value as? String {
                 if let storedImage = WebImageCache.shared.image(with: self.userId) {
                     self.profileImageView.image = storedImage
+                    self.updateTableHeaderHeight()
                 } else {
                     _ = WebImageCache.shared.downloadImage(at: url) {imageData in
                         DispatchQueue.main.async {
                             WebImageCache.shared.storeImage(image: imageData, withIdentifier: self.userId)
                             self.profileImageView.image = imageData
+                            self.updateTableHeaderHeight()
                         }
                     }
                 }
@@ -59,16 +66,30 @@ class MeViewController: UIViewController {
         let aboutRef = userBasicRef.child("about")
         aboutRef.observeSingleEvent(of: .value, with: {(snapshot) in
             self.aboutMeLabel.text = snapshot.value as? String ?? ""
+            self.updateTableHeaderHeight()
         })
         let followerCountRef = userRef.child("follower_count")
         followerCountRef.observeSingleEvent(of: .value, with: {(snapshot) in
             let followers = snapshot.value as? Int ?? 0
             self.followersCountLabel.text = "\(followers) Follower\(followers != 1 ? "s": "")"
+            self.updateTableHeaderHeight()
         })
     }
     
     @IBAction func followButtonTapped(_ sender: UIButton) {
         print("follow button tapped")
+    }
+    
+    func updateTableHeaderHeight() {
+        tableView.tableHeaderView?.frame.size.height = aboutMeLabel.frame.origin.y + aboutMeLabel.intrinsicContentSize.height + 8
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
     
 }
