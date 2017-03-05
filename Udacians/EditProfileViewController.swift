be2d187f88a79e69a4d4da902779404bc49d17d0
OwenLaRosa <owen.larosa@outlook.com>
2017-03-05 15:26:33 -0500

    feat: Add edit profile view controller

---
+++ Udacians/EditProfileViewController.swift
@@ -0,0 +1,22 @@
+//
+//  EditProfileViewController.swift
+//  Udacians
+//
+//  Created by Owen LaRosa on 3/5/17.
+//  Copyright © 2017 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class EditProfileViewController: UIViewController {
+    
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
