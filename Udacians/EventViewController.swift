2ed07980c862a1781d350e99974cdb80669102fd
OwenLaRosa <owen.larosa@outlook.com>
2017-03-10 16:07:20 -0500

    feat: Populate event info labels

--- Udacians/EventViewController.swift
+++ Udacians/EventViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class EventViewController: UIViewController {
     
@@ -17,11 +18,23 @@ class EventViewController: UIViewController {
     @IBOutlet weak var aboutLabel: UILabel!
     
     var eventId: String!
+    var ref: FIRDatabaseReference!
+    var eventRef: FIRDatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = eventId
+        ref = FIRDatabase.database().reference()
+        eventRef = ref.child("events").child(eventId)
+        let infoRef = eventRef.child("info")
+        infoRef.observe(.value, with: {(snapshot) in
+            guard let value = snapshot.value as? [String: Any] else { return }
+            self.nameLabel.text = value["name"] as? String ?? ""
+            self.timeAndPlaceLabel.text = value["place"] as? String ?? ""
+            self.aboutLabel.text = value["about"] as? String ?? ""
+        })
+        
+        
     }
     
     @IBAction func interactButtonTapped(_ sender: UIButton) {
