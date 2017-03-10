6039add6048be826adbc308543d5fa3e8389dc3b
OwenLaRosa <owen.larosa@outlook.com>
2017-03-10 15:45:14 -0500

    feat: Add event VC with table header

---
+++ Udacians/EventViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EventViewController.swift
+//  Udacians
+//
+//  Created by Owen LaRosa on 3/10/17.
+//  Copyright © 2017 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+
+class EventViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeAndPlaceLabel: UILabel!
+    @IBOutlet weak var attendeeCountLabel: UILabel!
+    @IBOutlet weak var interactButton: UIButton!
+    @IBOutlet weak var aboutLabel: UILabel!
+    
+    @IBAction func interactButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+}
