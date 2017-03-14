277f937bb3ce04bc19924485e9344c4bdf506286
OwenLaRosa <owen.larosa@outlook.com>
2017-03-14 19:27:43 -0400

    feat: Add view for annotating the map

---
+++ Udacians/MultipleInputViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MultipleInputViewController.swift
+//  Udacians
+//
+//  Created by Owen LaRosa on 3/14/17.
+//  Copyright © 2017 Owen LaRosa. All rights reserved.
+//
+
+import UIKit
+
+class MultipleInputViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var instructionLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var negativeButton: UIButton!
+    @IBOutlet weak var positiveButton: UIButton!
+
+    @IBAction func negativeButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func positiveButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
