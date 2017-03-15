e1acd2df91aa1b1b6a27f62fdd629eabe89c3c4d
OwenLaRosa <owen.larosa@outlook.com>
2017-03-15 14:42:32 -0400

    feat: Set button text based on page / input type

--- Udacians/MultipleInputViewController.swift
+++ Udacians/MultipleInputViewController.swift
@@ -17,8 +17,14 @@ class MultipleInputViewController: UIViewController {
     @IBOutlet weak var negativeButton: UIButton!
     @IBOutlet weak var positiveButton: UIButton!
     
+    static let BUTTON_CANCEL = "Cancel"
+    static let BUTTON_SUBMIT = "Submit"
+    static let BUTTON_BACK = "Back"
+    static let BUTTON_NEXT = "Next"
+    
     var contentType: ContentType!
     var coordinate: CLLocationCoordinate2D!
+    var currentPage = 1
     
     public enum ContentType: Int {
         case topic = 0, article, event
@@ -29,7 +35,36 @@ class MultipleInputViewController: UIViewController {
     }
     
     @IBAction func positiveButtonTapped(_ sender: UIButton) {
-        
+        switch contentType.rawValue {
+        case 0:
+            // topics only have one page
+            negativeButton.title = MultipleInputViewController.BUTTON_CANCEL
+            positiveButton.title = MultipleInputViewController.BUTTON_SUBMIT
+            break
+        case 1:
+            if currentPage == 1 {
+                negativeButton.title = MultipleInputViewController.BUTTON_CANCEL
+                positiveButton.title = MultipleInputViewController.BUTTON_NEXT
+            } else { // page 2
+                negativeButton.title = MultipleInputViewController.BUTTON_BACK
+                positiveButton.title = MultipleInputViewController.BUTTON_SUBMIT
+            }
+            break
+        case 2:
+            if currentPage == 1 {
+                negativeButton.title = MultipleInputViewController.BUTTON_CANCEL
+                positiveButton.title = MultipleInputViewController.BUTTON_NEXT
+            } else if currentPage == 2 {
+                negativeButton.title = MultipleInputViewController.BUTTON_BACK
+                positiveButton.title = MultipleInputViewController.BUTTON_NEXT
+            } else { // page 3
+                negativeButton.title = MultipleInputViewController.BUTTON_BACK
+                positiveButton.title = MultipleInputViewController.BUTTON_SUBMIT
+            }
+            break
+        default:
+            break
+        }
     }
     
 }
